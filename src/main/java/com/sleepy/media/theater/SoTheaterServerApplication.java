d4625c9690dd859481c6f85bf089bb06d446570b
sleepyocean <2913815267@qq.com>
2020-11-19 09:34:17 +0800

    init: 初始化提交

---
+++ src/main/java/com/sleepy/media/theater/SoTheaterServerApplication.java
@@ -0,0 +1,13 @@
+package com.sleepy.media.theater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoTheaterServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoTheaterServerApplication.class, args);
+    }
+
+}
